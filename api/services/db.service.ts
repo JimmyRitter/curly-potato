2722db56bbcbbee124a783e1b27c3de573d3047b
Jimmy Ritter Fontes <jimmyritter@kpmg.com.mt>
2023-02-10 15:11:31 +0100

    mongo setup, creation of auth related service and controller

---
+++ api/services/db.service.ts
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+import { dbConfig } from "../config/db";
+
+export const connectToDb = async (): Promise<any> => {
+  const client = await MongoClient.connect(dbConfig.connectionString);
+  const db = client.db();
+
+  return { client, db };
+};
+
+export const closeDbConnection = async (client: MongoClient): Promise<void> => {
+  try {
+    await client.close();
+  } catch (error) {
+    console.error("Error closing the database connection:", error);
+    throw error;
+  }
+};
