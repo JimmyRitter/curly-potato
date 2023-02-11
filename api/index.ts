7a2dd76fe623f8ee668d2b56e2e9a0892f3513db
Jimmy Ritter Fontes <jimmyritter@kpmg.com.mt>
2023-02-11 14:14:49 +0100

    integration between back and front end

--- api/index.ts
+++ api/index.ts
@@ -2,15 +2,19 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import { AuthRoute } from "./routes";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
+app.use(cors());
 const port = process.env.PORT;
 app.use(bodyParser.json());
 
 // Routes
 app.use("/auth", AuthRoute);
+
+// app.use(cors({ origin: "http://localhost:5173/" }));
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
