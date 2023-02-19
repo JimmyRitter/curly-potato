f42b59077cf476aa058bf8dd23bd41d53f10866e
Jimmy Ritter Fontes <jimmyritter@kpmg.com.mt>
2023-02-19 19:11:30 +0100

    fix vite.config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,18 +5,18 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "");
-  console.log(
-    "path.resolve(__dirname, '../web'),",
-    path.resolve(__dirname, "../web")
-  );
+  // const env = loadEnv(mode, process.cwd(), "");
+  // console.log(
+  //   "path.resolve(__dirname, '../web'),",
+  //   path.resolve(__dirname, "../web")
+  // );
 
   return {
     // vite config
     plugins: [react()],
-    define: {
-      VITE_API_URL: env.VITE_API_URL,
-    },
-    root: path.resolve(__dirname, "../web"),
+    // define: {
+    //   VITE_API_URL: env.VITE_API_URL,
+    // },
+    // root: path.resolve(__dirname, "../web"),
   };
 });
